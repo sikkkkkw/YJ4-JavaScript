abf0e7786108ee2ef36851f0805c4ccf6b19ec23
sikkkkkw <gksmsla92@naver.com>
2023-11-10 09:23:07 +0900

    test

---
+++ operator.js
@@ -0,0 +1,21 @@
+let x = 11;
+let y = 3;
+document.write("x + y =" + (x + y) + "<br>");
+document.write("x - y =" + (x - y) + "<br>");
+document.write("x * y =" + x * y + "<br>");
+document.write("x / y =" + x / y + "<br>");
+document.write("x % y =" + x % y + "<br>");
+document.write(parseInt(x/y));
+
+let t = 10;
+let z = 10;
+let g = 10;
+
+t = t - 5;
+document.write(t + "<br>");
+z -= 5;
+document.write(z + "<br>");
+g -= 5;
+document.write(g + "<br>");
+g =- 5;
+document.write(g);
