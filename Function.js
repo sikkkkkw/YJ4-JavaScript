825963b6c5063ef104709d89cb925647386c47ec
sikkkkkw <gksmsla92@naver.com>
2023-11-13 11:59:08 +0900

    2023-11-13

---
+++ Function.js
@@ -0,0 +1,39 @@
+// 자바스크립트 내장함수
+// parseInt() 문자열에서 정수형로 반환
+// parseFloat() 문자열에서 실수형 반환
+// String() 문자형으로 반환
+// Number() 숫자형으로 반환
+// Boolean() 논리형으로 반환
+
+
+//기명 함수 선언식
+function addNum(x,y){
+    document.write(parseFloat(x)+y+"<br>");
+    document.write(x-y+"<br>");
+}
+addNum("2.5",3);
+addNum(10);//인자값이 1개라서 NaN
+//화살표 함수(function 대신에 =>사용)
+let addNum1 = (x,y)=>{
+    return x+y;
+}
+document.write(addNum1(2,3)+"<br>");
+
+let addNum2 = (x,y) => x+y;
+document.write(addNum2(2,3)+"<br>");
+//반환문(Return)과 매개변수(Parameter)
+function multiNum(x,y){
+    return x*y;
+}
+var num = multiNum(3,4);
+document.write(num+"<br>");//변수 num 값 출력
+document.write(multiNum(3)+"<br>");//인수로 3을 전달하여 함수를 호출함,->NaN
+// 재귀함수 정의문
+function count(x){
+    if(x === 0){
+        document.write("함수 실행 종료");
+    }else{
+        document.write("count x값:"+x+"<br>");
+        count(x-1);
+    }
+}count(5);
--- for.js
+++ for.js
@@ -30,3 +30,66 @@ for (let i = 1; i <= 10; i++) {
     document.write("*");
 }document.write("<br>");
 }
+// 피라미드 반대
+for (let i = 1; i <= 10; i++){
+    for(let k = 1; k <= 10 - i; k++){
+        document.write("*");
+    }
+    document.write("<br>");
+}
+//for/in문
+//<일반적인 for의 문법>
+let arr = [3,4,5];
+for(let i =0;i<arr.length; i++){
+    document.write(i+"번째 = ");
+    document.write(arr[i]+" ");
+}document.write("<br>");
+// <for/in의 문법>
+for(let i in arr){
+    document.write(i+"번째 = ");
+    document.write(arr[i]+" ");
+}document.write("<br>");
+//for/of문
+//<for/of의 문법>
+for(let item of arr){
+    document.write(item+" ");
+}document.write("<br>");
+// while문
+let i =1;
+while(i <= 10){
+    document.write(i+" ");
+    i++
+}document.write("<br>");
+//while문으로 1부터 10까지 합 구하는 프로그램
+while(i <= 10){
+    sum+=i;
+    i++;
+}
+document.write(`1부터 10까지의 합 ${sum}입니다.`)
+document.write("<br>")
+//do/while문
+let id =1;//전역변수
+do{
+    document.write(id+" ");
+    id++
+}while(id <= 10)
+document.write("<br>");
+//break문
+let r,sum2=0;
+for(r = 1; r <= 100; r++){
+    sum2 += r;
+    if(sum2 >= 10)// 합계가 10이상일 경우 반복문 종료
+    break;
+}
+document.write(`최초로 합이 10 이상인 위치 ${r}, 합은 ${sum2}`);
+document.write("<br>")
+// continue문
+let t,sum3=0;
+for(t=1; t<=10; t++){
+    if(t % 2 ==0)
+    continue;
+    sum3 += t;
+
+}
+document.write(`1에서 10까지 홀수의 합은 ${sum3}`);
+document.write("<br>");
